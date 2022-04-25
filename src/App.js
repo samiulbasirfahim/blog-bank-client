623b7a930bb3bf7b00d55c2cb285bdc0d34f97b0
samiulbasirfahim <samiulbasirfahim@hotmail.com>
2022-04-26 03:28:43 +0600

    starter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { useEffect, useState } from "react"
-import "./App.css"
 import useSetDarkMode from "./hooks/useSetDarkMode"
 
 function App() {
-  const {isDarkMode, handleDarkMode} = useSetDarkMode()
+	const { isDarkMode, handleDarkMode } = useSetDarkMode()
 	return (
 		<div className={`${isDarkMode && "dark"}`}>
-      <button onClick={handleDarkMode}>toogle dark and white mode</button>
+			<button onClick={handleDarkMode}>toogle dark and white mode</button>
 			<h1 className="text-red-800 dark:text-green-800">hello world!</h1>
 		</div>
 	)
