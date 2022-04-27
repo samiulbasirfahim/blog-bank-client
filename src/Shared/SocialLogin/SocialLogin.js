ad302f7d18a70b1571af55307f5a67d7f5b35e06
samiulbasirfahim <samiulbasirfahim@hotmail.com>
2022-04-27 20:26:32 +0600

    theme bug fixed

--- src/Shared/SocialLogin/SocialLogin.js
+++ src/Shared/SocialLogin/SocialLogin.js
@@ -11,13 +11,13 @@ const SocialLogin = () => {
 	return (
 		<div className="w-[95%] md:w-2/4 grid">
 			<button
-				className="bg-gray-200 dark:bg-gray-600 mt-8 py-4 rounded-md "
+				className="bg-white dark:bg-gray-600 mt-8 py-4 rounded-md "
 				onClick={() => googleSignIn()}
 			>
 				Continue with google
 			</button>
 			<button
-				className="bg-gray-200 dark:bg-gray-600 mt-8 py-4 rounded-md "
+				className="bg-white dark:bg-gray-600 mt-8 py-4 rounded-md "
 				onClick={() => githubSignIn()}
 			>
 				Continue with github
